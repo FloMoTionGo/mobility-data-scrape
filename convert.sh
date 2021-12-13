621b4d8d47aa6d1416f837e57cd4ececad6035ee
Florian Motz <florianmotz@outlook.com>
2021-12-13 14:35:32 +0100

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -1,21 +1,11 @@
 touch  CSV_per_day/_.csv
 
-query_date=$(date +"%y%m")
-service=vbb
+query_date=$(bash date -d "Today -5 days" +"%y%m%d")
 
-jq .[]."tripId" JSON_source/$query_date*$service*.json | awk '{for(i=1;i<=NF;i++)a[i][NR]=$i}END{for(i in a)for(j in a[i])printf"%s"(j==NR?"\n":FS),a[i][j]}' FS=, >> CSV_per_day/_$query_date$service.csv
-jq .[]."when" JSON_source/$query_date*$service*.json | awk '{for(i=1;i<=NF;i++)a[i][NR]=$i}END{for(i in a)for(j in a[i])printf"%s"(j==NR?"\n":FS),a[i][j]}' FS=, >> CSV_per_day/_$query_date$service.csv
-jq .[]."line"."id" JSON_source/$query_date*$service*.json | awk '{for(i=1;i<=NF;i++)a[i][NR]=$i}END{for(i in a)for(j in a[i])printf"%s"(j==NR?"\n":FS),a[i][j]}' FS=, >> CSV_per_day/_$query_date$service.csv
-jq .[]."line"."mode" JSON_source/$query_date*$service*.json | awk '{for(i=1;i<=NF;i++)a[i][NR]=$i}END{for(i in a)for(j in a[i])printf"%s"(j==NR?"\n":FS),a[i][j]}' FS=, >> CSV_per_day/_$query_date$service.csv
-awk '{for(i=1;i<=NF;i++)a[i][NR]=$i}END{for(i in a)for(j in a[i])printf"%s"(j==NR?"\n":FS),a[i][j]}' FS=, CSV_per_day/_$query_date$service.csv > CSV_per_day/$query_date$service.csv
+service=vbb
+jq '.[] | [ .tripId, .when, .stop.location.latitude, .stop.location.longitude ] | @csv' JSON_source/$query_date*$service*.json  >> CSV_per_day/$query_date$service.csv
 
 service=TIER
-
-jq .data[]."id" JSON_source/$query_date*$service*.json | awk '{for(i=1;i<=NF;i++)a[i][NR]=$i}END{for(i in a)for(j in a[i])printf"%s"(j==NR?"\n":FS),a[i][j]}' FS=, >> CSV_per_day/_$query_date$service.csv
-jq .data[]."attributes"."lastLocationUpdate" JSON_source/$query_date*$service*.json | awk '{for(i=1;i<=NF;i++)a[i][NR]=$i}END{for(i in a)for(j in a[i])printf"%s"(j==NR?"\n":FS),a[i][j]}' FS=, >> CSV_per_day/_$query_date$service.csv
-jq .data[]."attributes"."vehicleType" JSON_source/$query_date*$service*.json | awk '{for(i=1;i<=NF;i++)a[i][NR]=$i}END{for(i in a)for(j in a[i])printf"%s"(j==NR?"\n":FS),a[i][j]}' FS=, >> CSV_per_day/_$query_date$service.csv
-jq .data[]."attributes"."lat" JSON_source/$query_date*$service*.json | awk '{for(i=1;i<=NF;i++)a[i][NR]=$i}END{for(i in a)for(j in a[i])printf"%s"(j==NR?"\n":FS),a[i][j]}' FS=, >> CSV_per_day/_$query_date$service.csv
-jq .data[]."attributes"."lng" JSON_source/$query_date*$service*.json | awk '{for(i=1;i<=NF;i++)a[i][NR]=$i}END{for(i in a)for(j in a[i])printf"%s"(j==NR?"\n":FS),a[i][j]}' FS=, >> CSV_per_day/_$query_date$service.csv
-awk '{for(i=1;i<=NF;i++)a[i][NR]=$i}END{for(i in a)for(j in a[i])printf"%s"(j==NR?"\n":FS),a[i][j]}' FS=, CSV_per_day/_$query_date$service.csv > CSV_per_day/$query_date$service.csv
+jq '.data[] | [ .attributes.lastLocationUpdate, .attributes.vehicleType, .attributes.lat, .attributes.lng  ] | @csv' JSON_source/$query_date*$service*.json  >> CSV_per_day/$query_date$service.csv
 
 rm CSV_per_day/_*.csv
