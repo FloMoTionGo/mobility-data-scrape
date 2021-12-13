9311e666c1320a1ff4dfe80372403cf610bd29d1
Florian Motz <florianmotz@outlook.com>
2021-12-13 18:36:07 +0100

    summary
    change

--- convert.sh
+++ convert.sh
@@ -1,11 +1,5 @@
-touch  CSV_per_day/_.csv
-
-query_date=$(bash date -d "Today -5 days" +"%y%m%d")
-
+query_date=$(date -d "Today -4 days" +"%y%m%d")
 service=vbb
-jq '.[] | [ .tripId, .when, .stop.location.latitude, .stop.location.longitude ] | @csv' JSON_source/$query_date*$service*.json  >> CSV_per_day/$query_date$service.csv
-
+jq '.[] | [ .tripId, .when, .stop.location.latitude, .stop.location.longitude ] | @csv' JSON_source/${query_date}*${service}*.json > CSV_per_day/${query_date}${service}.csv
 service=TIER
-jq '.data[] | [ .attributes.lastLocationUpdate, .attributes.vehicleType, .attributes.lat, .attributes.lng  ] | @csv' JSON_source/$query_date*$service*.json  >> CSV_per_day/$query_date$service.csv
-
-rm CSV_per_day/_*.csv
+jq '.data[] | [ .attributes.lastLocationUpdate, .attributes.vehicleType, .attributes.lat, .attributes.lng  ] | @csv' JSON_source/$query_date*$service*.json > CSV_per_day/${query_date}${service}.csv
