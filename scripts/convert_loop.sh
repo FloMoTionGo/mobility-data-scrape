7b16b0adc6038ab79b7a255f2ef41e32d22caf5c
Florian Motz <florianmotz@outlook.com>
2021-12-15 20:26:50 +0100

    update CSVs
    update daily csv

---
+++ scripts/convert_loop.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+for d in {211208..211213}
+do
+echo $d
+service=vbb
+jq -r '.[] | [ .tripId, .when, .stop.location.latitude, .stop.location.longitude ] | @csv' JSON_source/${d}*${service}*.json > CSV_per_day/${d}${service}.csv
+service=TIER
+jq -r '.data[] | [ .attributes.lastLocationUpdate, .attributes.vehicleType, .attributes.lat, .attributes.lng  ] | @csv' JSON_source/${d}*${service}*.json > CSV_per_day/${d}${service}.csv
+done;
