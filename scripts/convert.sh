756fd000efe395c6d1bed7ff56ee17276ea35878
Florian Motz <florianmotz@outlook.com>
2021-12-15 20:29:30 +0100

    update CSV2

--- scripts/convert.sh
+++
@@ -1,5 +0,0 @@
-query_date=$(date -d "Today -4 days" +"%y%m%d")
-service=vbb
-jq '.[] | [ .tripId, .when, .stop.location.latitude, .stop.location.longitude ] | @csv' JSON_source/${query_date}*${service}*.json > CSV_per_day/${query_date}${service}.csv
-service=TIER
-jq '.data[] | [ .attributes.lastLocationUpdate, .attributes.vehicleType, .attributes.lat, .attributes.lng  ] | @csv' JSON_source/$query_date*$service*.json > CSV_per_day/${query_date}${service}.csv
--- scripts/convert_loop.sh
+++ scripts/convert_loop.sh
@@ -1,9 +1,9 @@
 #!bin/bash
-for d in {211208..211213}
+for d in {211208..211214}
 do
 echo $d
 service=vbb
-jq -r '.[] | [ .tripId, .when, .stop.location.latitude, .stop.location.longitude ] | @csv' JSON_source/${d}*${service}*.json > CSV_per_day/${d}${service}.csv
+jq -r '.[] | [ .tripId, .when, .stop.location.latitude, .stop.location.longitude ] | @csv' ./JSON_source/${d}*${service}*.json > ./CSV_per_day/${d}${service}.csv
 service=TIER
-jq -r '.data[] | [ .attributes.lastLocationUpdate, .attributes.vehicleType, .attributes.lat, .attributes.lng  ] | @csv' JSON_source/${d}*${service}*.json > CSV_per_day/${d}${service}.csv
+jq -r '.data[] | [ .attributes.lastLocationUpdate, .attributes.vehicleType, .attributes.lat, .attributes.lng  ] | @csv' ./JSON_source/${d}*${service}*.json > ./CSV_per_day/${d}${service}.csv
 done;
