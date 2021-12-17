4529840dacc46ef25fd16a9853a47f2177c6c10d
Florian Motz <florianmotz@outlook.com>
2021-12-17 17:11:03 +0100

    update workflow
    move files to test workflow in workflow folder

--- scripts/import_vbb_TIER.sh
+++ scripts/import_vbb_TIER.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-db_path='../DB/vbb.sqlite'
+db_path='../DB/mobility.sqlite'
 created_at=$(date -d "yesterday" +%y%m%d)
 
-sqlite3 ${db_path} < ../scripts/generate_DB.sqlite #> $@
-sqlite3 ${db_path} -cmd ".header on" ".mode csv" ".import ../CSV_per_day/${created_at}vbb.csv vbb" > $db_path
-# ".output $io_path " ".mode column" "SELECT * FROM vbb"
+sqlite3 ${db_path} < ../scripts/generate_DB.sqlite
+sqlite3 ${db_path} -cmd ".header on" ".mode csv" ".import ../CSV_per_day/${created_at}vbb.csv vbb"
+sqlite3 ${db_path} -cmd ".header on" ".mode csv" ".import ../CSV_per_day/${created_at}TIER.csv TIER"
