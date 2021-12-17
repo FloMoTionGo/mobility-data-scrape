eb3fd59c1b70042fcd470877d9fcbff3325a85c5
Florian Motz <florianmotz@outlook.com>
2021-12-17 16:52:20 +0100

    update test.yml
    update test.yml

--- scripts/import_vbb_TIER.sh
+++ scripts/import_vbb_TIER.sh
@@ -2,6 +2,6 @@
 db_path='../DB/vbb.sqlite'
 created_at=$(date -d "yesterday" +%y%m%d)
 
-sqlite3 ${db_path} < generate_DB.sqlite #> $@
-sqlite3 ${db_path} -cmd ".header on" ".mode csv" ".import ../CSV_per_day/${created_at}vbb.csv vbb"
+sqlite3 ${db_path} < ../scripts/generate_DB.sqlite #> $@
+sqlite3 ${db_path} -cmd ".header on" ".mode csv" ".import ../CSV_per_day/${created_at}vbb.csv vbb" > $db_path
 # ".output $io_path " ".mode column" "SELECT * FROM vbb"
