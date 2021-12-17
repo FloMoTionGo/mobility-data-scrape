625d1fca46f3ca7775b787bdf46693a069d7593b
Florian Motz <florianmotz@outlook.com>
2021-12-17 14:29:51 +0100

    Update test.sh
    complete SQL statement

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,9 @@
 output_path='./DB/test.db'
 echo "
 CREATE TABLE vbb(
+   id varchar(255),
+   arrival varchar(255),
+   latitude varchar(255),
+   longitude varchar(255)
+   );
 " | sqlite3 $output_path
