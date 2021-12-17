0d611b0c1332ea75c4874831823fc78f8d07df9f
Florian Motz <florianmotz@outlook.com>
2021-12-17 14:23:34 +0100

    Summary
    new struct

---
+++ scripts/sqlite3_generate_db.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+output_path='../DB/test.db'
+echo "
+CREATE TABLE vbb(
+   id varchar(255),
+   arrival varchar(255),
+   latitude varchar(255),
+   longitude varchar(255)
+   );
+
+.mode csv
+.import ../CSV_per_day/211208vbb.csv vbb
+.import ../CSV_per_day/211209vbb.csv vbb
+.import ../CSV_per_day/211210vbb.csv vbb
+.import ../CSV_per_day/211211vbb.csv vbb
+.import ../CSV_per_day/211212vbb.csv vbb
+.import ../CSV_per_day/211213vbb.csv vbb
+.import ../CSV_per_day/211214vbb.csv vbb
+.import ../CSV_per_day/211215vbb.csv vbb
+.import ../CSV_per_day/211216vbb.csv vbb
+
+.mode column
+.output vbb.db
+
+SELECT id,
+        SUBSTR(id,3,5) as trainID,
+        arrival,
+        latitude,
+        longitude
+FROM vbb;
+
+--.output stdout.sql
+--.dump
+--.exit
+" | sqlite3 $output_path
+#EOL
