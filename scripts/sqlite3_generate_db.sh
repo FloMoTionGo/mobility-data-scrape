6ddc6df099cc2b77515eea10130cec0422e12a87
Florian Motz <florianmotz@outlook.com>
2021-12-17 14:48:44 +0100

    apply path change

--- scripts/sqlite3_generate_db.sh
+++ scripts/sqlite3_generate_db.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-output_path='./DB/vbb.db'
+output_path='../DB/vbb.db'
 echo "
 CREATE TABLE vbb(
    id varchar(255),
@@ -9,18 +9,19 @@ CREATE TABLE vbb(
    );
 
 .mode csv
-.import ./CSV_per_day/211208vbb.csv vbb
-.import ./CSV_per_day/211209vbb.csv vbb
-.import ./CSV_per_day/211210vbb.csv vbb
-.import ./CSV_per_day/211211vbb.csv vbb
-.import ./CSV_per_day/211212vbb.csv vbb
-.import ./CSV_per_day/211213vbb.csv vbb
-.import ./CSV_per_day/211214vbb.csv vbb
-.import ./CSV_per_day/211215vbb.csv vbb
-.import ./CSV_per_day/211216vbb.csv vbb
+.import ../CSV_per_day/211208vbb.csv vbb
+.import ../CSV_per_day/211209vbb.csv vbb
+.import ../CSV_per_day/211210vbb.csv vbb
+.import ../CSV_per_day/211211vbb.csv vbb
+.import ../CSV_per_day/211212vbb.csv vbb
+.import ../CSV_per_day/211213vbb.csv vbb
+.import ../CSV_per_day/211214vbb.csv vbb
+.import ../CSV_per_day/211215vbb.csv vbb
+.import ../CSV_per_day/211216vbb.csv vbb
 
 .mode column
 .output $output_path
+.header on
 SELECT id,
         SUBSTR(id,3,5) as trainID,
         arrival,
